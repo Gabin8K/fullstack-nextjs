9d1904bdf655803d10c4ff0e5612641d3d61f01b
Gabin8K <gabindjomo21@gmail.com>
2024-05-09 22:43:56 -0400

    adding nextauth google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import prisma from "@/lib/prisma";
-import Image from "next/image";
-
-export const handleubmit = async(id:string)=>{
-  
-}
-
 export default function Home() {
   return (
     <main>
