de6b5485d2f046db9c056ad0e16ca350a2c74fcb
Gabin8K <gabindjomo21@gmail.com>
2024-05-11 10:51:17 -0400

    fix type

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,6 +4,6 @@ import { DefaultSession } from "next-auth";
 
 declare module 'next-auth' {
   interface Session {
-    user: Prisma.userGetPayload<{}> & DefaultSession['user']
+    user: Prisma.userGetPayload<{}>
   }
 }
